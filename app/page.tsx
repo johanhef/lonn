0dfe7437027395b11becb3d6e56fbd64fff7dae1
Johan-Henrik Fylling <fyllingjohan@gmail.com>
2025-01-25 16:23:31 +0100

    Remove min height

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SalaryCalculator from "./SalaryCalculator";
 export default function Home() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <div className="flex items-center justify-center">
           <Image
